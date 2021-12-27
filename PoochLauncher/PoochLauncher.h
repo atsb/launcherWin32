98c46f65213c435f0950251af95371857e6433e2
Gibbon <atsb@atsb>
2021-12-27 09:54:11 +0100

    Adding Pooch Launcher

---
+++ PoochLauncher/PoochLauncher.h
@@ -0,0 +1,39 @@
+#pragma once
+
+/*
+    Pooch Launcher - a minimalist WIN32 API launcher for the Pooch Source Port
+    Copyright (C) 2021  Adam Bilbrough
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "resource.h"
+
+// This file hold the global variables in-use in SprinkledLauncher.cpp
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+
+// Help text
+const wchar_t* help = L"   Commands:\n   -iwad <- the path to your DOOM.WAD/DOOM2.WAD/TNT.WAD etc...\n   -file <- the path to any PWAD files or .deg files to load alongside the IWAD\n   -skill <integer> <- 1 - 5 in order of difficulty.  Example (-skill 4 <- is ultra violence)\n   -warp <- level to warp to on game start (-warp 1 1 <- E1M1 for DOOM or -warp 7 <- MAP07 for DOOM2)\n   -fullscreen <- forces fullscreen mode. Alternaitvely -window forces windowed mode.\n   -record <name> <- record a game. Example (-record test)\n   -playdemo <name> <- plays back a recorded demo. Example (-playdemo test)\n\n   Command line arguments:\n";
+
+// The window for the Command Line Arguments edit control
+HWND CommandLineArgs;
+
+// The integer for the length of our argument string
+int argTextLength;
+
+// The string that we will use to get our arguments
+LPTSTR argText;
---
+++ PoochLauncher/Resource.h
@@ -0,0 +1,54 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by SprinkledLauncher.rc
+
+/*
+    Pooch Launcher - a minimalist WIN32 API launcher for the Pooch Source Port
+    Copyright (C) 2021  Adam Bilbrough
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_POOCHLAUNCHER_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_POOCHLAUNCHER			107
+#define IDI_SMALL				108
+#define IDC_POOCHLAUNCHER			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+
+//Buttons
+#define ID_POOCH_BUTTON			200
+#define ID_POOCHACC_BUTTON		300
+
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
